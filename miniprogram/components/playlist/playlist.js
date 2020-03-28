84fcc745a05b25539767de5fb1a20ff27bfd8284
沈忱 <sshencchen@sina.com>
2020-03-28 23:49:23 +0800

    细节

--- miniprogram/components/playlist/playlist.js
+++ miniprogram/components/playlist/playlist.js
@@ -9,6 +9,18 @@ Component({
     }
   },
 
+  /* 数据监听器 */
+  observers: {
+    // playlist(val) { 监听对象
+    //   console.log(val);
+    // }
+    // 监听对象下面的属性
+    ['playlist.playCount'](count) {
+      console.log(count);
+      console.log(this.handleNumber(count, 2));
+    }
+  },
+
   /**
    * 组件的初始数据 组件内部数据
    */
@@ -20,6 +32,15 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    // 保留到小数点后几位
+    handleNumber(num, point) {
+      let numStr = num.toString().split('.')[0];
+      if(numStr.length < 6) { // 10w以内
+        return numStr;
+      } else if (numStr.length >= 6 && numStr.length <= 8) { // 10w以上百万以内
+        let decimal = numStr.substring(numStr.length - 4, numStr.length - 4 + point);
+        return parseFloat(parseInt(num / 10000) + '.' + decimal) + '万';
+      }
+    }
   }
 })
